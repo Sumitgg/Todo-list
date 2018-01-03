611a87cbd8fd3ac863128e9b3c8bcccca476bb09
Sumit Ganju <sganju@Sumits-MacBook-Pro.local>
2018-01-03 13:14:13 -0500

    Connected AddTaskViewController to the app functionality to add tasks

--- Todo list/AddTaskViewController.swift
+++ Todo list/AddTaskViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class AddTaskViewController: UIViewController {
-
+   
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var isImp: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+    @IBAction func btnTapped(_ sender: Any) {
+        
+     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+     let task = Task(context: context)
+     task.name = textField.text!
+     task.isImportant = isImp.isOn
+        
+      //Save the data to coredata
+        
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+    navigationController!.popViewController(animated: true)
+        
+    }
 }
