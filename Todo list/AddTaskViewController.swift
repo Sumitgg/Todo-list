abe174488d64dccf5f9a08a297568634a9f651d5
Sumit Ganju <sganju@Sumits-MacBook-Pro.local>
2018-01-03 12:11:06 -0500

    UI controls completed for the App

---
+++ Todo list/AddTaskViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddTaskViewController.swift
+//  Todo list
+//
+//  Created by Sumit Ganju on 2018-01-03.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
